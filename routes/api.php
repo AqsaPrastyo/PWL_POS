38200e1053db29d5aac80a35e529b3278ff39e81
DrWallace <aqsaherryprasetya@gmail.com>
2025-05-04 16:27:46 +0700

    feat: implement logout functionality with JWT authentication, including LogoutController and route updates (Jobsheet 10 practicum 3)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LoginController;
+
 
 
 
@@ -11,7 +14,7 @@ Route::post('/login', \App\Http\Controllers\Api\LoginController::class)->name('l
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-    
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
 
 // Route::get('/user', function (Request $request) {
