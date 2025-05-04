119cd98784f33ff27e7c0ca2fbf7add5f5a157ae
DrWallace <aqsaherryprasetya@gmail.com>
2025-05-04 18:47:05 +0700

    feat: implement LevelController with CRUD operations for levels, including routes for index, store, show, update, and destroy  (Jobsheet 10 practicum 4

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\UserController;
 
 
 
@@ -15,6 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+Route::get('level', [LevelController::class, 'index']);
+Route::post('level', [LevelController::class, 'store']);
+Route::get('level/{level}', [LevelController::class, 'show']);
+Route::put('level/{level}', [LevelController::class, 'update']);
+Route::delete('level/{level}', [LevelController::class, 'destroy']);
 
 
 // Route::get('/user', function (Request $request) {
