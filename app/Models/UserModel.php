dd48ea08ecebab0f1ee74d89bf8c624316c69774
DrWallace <aqsaherryprasetya@gmail.com>
2025-05-05 09:15:03 +0700

    feat: add image field to user registration, update validation and model, and modify API route (Jobsheet 11 practicum 1)

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
@@ -22,7 +23,7 @@ class UserModel extends Authenticatable implements JWTSubject
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'image'];
 
     protected $hidden = ['password'];
 
@@ -31,6 +32,11 @@ class UserModel extends Authenticatable implements JWTSubject
     public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(get:fn ($image) => url('/storage/posts/' . $image));
     }
 
     public function stok()
